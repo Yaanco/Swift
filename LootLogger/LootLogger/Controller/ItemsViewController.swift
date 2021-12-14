88e8b0b78614386ffaf21252a6080d7e33971d5b
zhangycc <im.zhangycc@gmail.com>
2021-12-14 14:12:48 +0800

    添加LootLogger工程

---
+++ LootLogger/LootLogger/Controller/ItemsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ItemsViewController.swift
+//  LootLogger
+//
+//  Created by Yaanco on 2021/12/14.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
---
+++ LootLogger/LootLogger/View/ItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  ItemCell.swift
+//  LootLogger
+//
+//  Created by Yaanco on 2021/12/14.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
